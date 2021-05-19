923d184d90791b92f73248e25cdc3598631c4251
Coincoin-666 <olf_hrzg@live.fr>
2021-05-19 17:05:29 +0200

    catalogue js

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+const computers = [{
+    "brand": "asus",
+    "model": "XDF43",
+    "price": 239
+},
+{
+    "brand": "hp",
+    "model": "655JH",
+    "price": 654
+},
+{
+    "brand": "huawei",
+    "model": "chiQon",
+    "price": 458
+}
+];
+
+const smartphones = [{
+    "brand": "samsung",
+    "model": "s9",
+    "price": 656
+},
+{
+    "brand": "apple",
+    "model": "iphone8",
+    "price": 767
+},
+{
+    "brand": "nokia",
+    "model": "3310",
+    "price": 3310
+}
+];
+
+const tablets = [{
+    "brand": "apple",
+    "model": "ipadPro",
+    "price": 999
+},
+{
+    "brand": "samsung",
+    "model": "notepad",
+    "price": 678
+},
+{
+    "brand": "asus",
+    "model": "XDS",
+    "price": 770
+}
+];
+
+const products = [computers, smartphones, tablets];
+
+console.log(computers[1]);
+console.log(products[0]);
