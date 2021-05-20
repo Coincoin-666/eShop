17d7748e399c1c8b52314dc6fe6181e6932983a1
Bigends <chris.balkumar@outlook.fr>
2021-05-20 09:38:09 -0300

    les produits masqués

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+     jQuery(document).ready(function()
+     {
+     jQuery('.boxpc1').hide();
+     jQuery('a.pcportable').click(function()
+     {
+     jQuery('.boxpc1').toggle(700);
+     return false;
+     });
+     });
+
+
+     jQuery(document).ready(function()
+     {
+     jQuery('.boxpc2').hide();
+     jQuery('a.ranger2').click(function()
+     {
+     jQuery('.boxpc2').toggle(700);
+     return false;
+     });
+     });
+
+
+     jQuery(document).ready(function()
+     {
+     jQuery('.boxpc3').hide();
+     jQuery('a.ranger3').click(function()
+     {
+     jQuery('.boxpc3').toggle(700);
+     return false;
+     });
+     });
