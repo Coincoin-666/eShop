49b9b4c4e6b5d2dd8c7159791b5320e834021096
Coincoin-666 <olf_hrzg@live.fr>
2021-05-25 09:52:10 +0200

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,6 +105,16 @@
 			cartList.insertAdjacentHTML('beforeend', productAdded);
 		};
 
+		function addProduct(S21) {
+			// this is just a product placeholder
+			// you should insert an item with the selected product info
+			// replace productId, productName, price and url with your real product info
+			// you should also check if the product was already in the cart -> if it is, just update the quantity
+			$("#S21") = $("#S21") + 1;
+			var productAdded = '<li class="cd-cart__product"><div class="cd-cart__image"><a href="#0"><img src="assets/img/s21.png" alt="placeholder"></a></div><div class="cd-cart__details"><h3 class="truncate"><a href="#0">Samsung</a></h3><span class="cd-cart__price">949€</span><div class="cd-cart__actions"><a href="#0" class="cd-cart__delete-item">Supprimer</a><div class="cd-cart__quantity"><label for="cd-product-'+ productId +'">Qté</label><span class="cd-cart__select"><select class="reset" id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select><svg class="icon" viewBox="0 0 12 12"><polyline fill="none" stroke="currentColor" points="2,4 6,8 10,4 "/></svg></span></div></div></div></li>';
+			cartList.insertAdjacentHTML('beforeend', productAdded);
+		};
+
 		function removeProduct(product) {
 			if(cartTimeoutId) clearInterval(cartTimeoutId);
 			removePreviousProduct(); // prduct previously deleted -> definitively remove it from the cart
